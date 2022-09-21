8ee1d3d6e2093135891ae2c09fc9a2b4a20b4055
Cbih939 <memegold999@gmail.com>
2022-09-21 20:07:54 -0300

    property-biding e event-biding

--- src/app/segundo-componente/segundo-componente.component.ts
+++ src/app/segundo-componente/segundo-componente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivationStart } from '@angular/router';
 
 @Component({
   selector: 'app-segundo-componente',
@@ -8,4 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class SegundoComponenteComponent {
   nome = "Cbimoh Athayde";
   dataNascimento = "1989-09-06";
+  urlImagem = "/assets/avatar.jpg";
+
+  mostrarDataNascimento(){
+    alert(`A data de nascimento de Cbimoh é: ${this.dataNascimento}`);
+  }
 }
